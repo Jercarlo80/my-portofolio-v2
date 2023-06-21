205906e23d34de960a217cae364a82d11c2552d8
Jercarlo <75525322+Jercarlo80@users.noreply.github.com>
2023-06-22 03:30:25 +0700

    add-navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-     
+    <div className="">
+     <Navbar/>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,103 @@
+import React, { useState } from "react";
+import { FaBars, FaTimes } from 'react-icons/fa';
+
+
+const Navbar = () => {
+    const links = [
+        {
+            id: 1,
+            link: "HOME",
+        },
+        {
+            id: 2,
+            link: "ABOUT ME",
+        },
+        {
+            id: 3,
+            link: "EXPERIENCE",
+        },
+        {
+            id: 4,
+            link: "PORTOFOLIO",
+        },
+        {
+            id: 5,
+            link: "CONTACT",
+        }
+    ]
+    const [nav, setNav] = useState(false);
+    
+  return (
+    <div className='${navbarColor} flex justify-between items-center w-full h-20 px-4 fixed'>
+        <h1 className='text-[#0190ff] font-bold text-4xl cursor-pointer ml-3'>JERCARLO.</h1>
+        <ul className='hidden md:flex'>
+            {
+                links.map(({id, link}) => (
+                    <li 
+                        key={id} 
+                        className='
+                        px-4 
+                        text-gray-500 
+                        hover:scale-110 
+                        duration-200 
+                        hover:text-white 
+                        cursor-pointer
+                        
+                        '
+                        >
+                            {link}
+                    </li>
+                ))
+            }
+        </ul>
+
+        <div 
+        onClick={() => setNav(!nav)} 
+        className="cursor-pointer pr-4 z-10 text-white md:hidden">
+            {nav ? <FaTimes className="text-white" size={30}/> : <FaBars size={30}/>}
+            {nav && (
+                <ul className="
+                flex
+                flex-col
+                justify-center
+                items-center
+                absolute
+                top-0
+                left-0
+                w-full
+                h-screen
+                bg-gradient-to-b
+                from-black
+                to-gray-800
+                text-white
+
+            ">
+
+            {
+                links.map(({id, link}) => (
+                    <li 
+                        key={id} 
+                        className='
+                        px-4 
+                        py-6
+                        text-2xl
+                        text-gray-500 
+                        hover:scale-110 
+                        duration-200 
+                        hover:text-white 
+                        cursor-pointer
+                        '
+                        >
+                            {link}
+                    </li>
+                ))
+            }
+            </ul>
+            )}
+        </div>
+            
+    </div>
+  )
+}
+
+export default Navbar
